01943700d121c42dedde41106bae8e0213667fce
dongceha <524970335@qq.com>
2020-08-26 22:27:43 +0800

    first commit

---
+++ src/pages/Foo.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+      <h1>Foo page</h1>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'FooPage',
+    metaInfo: {
+        title:'foo'
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/templates/Post.vue
@@ -0,0 +1,31 @@
+<template>
+<Layout>
+  <div>
+      <h1>{{$page.post.title}}</h1>
+      <p>{{$page.post.content}}</p>
+  </div>
+</Layout>
+</template>
+<page-query>
+query($id: ID!) {
+  post(id: $id) {
+    id,
+    title,
+    content
+  }
+}
+</page-query>
+<script>
+export default {
+    name: 'Post',
+    metaInfo() {
+        return {
+            title: this.$page.post.title
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
