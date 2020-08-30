87a9cbaae3dbee0d12fcd72770e17b4c53f77f14
dongceha <524970335@qq.com>
2020-08-30 14:06:11 +0800

    增加文章

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -19,21 +19,32 @@
   <div class="container">
     <div class="row">
       <div class="col-lg-8 col-md-10 mx-auto">
-        <div class="post-preview">
+        <div class="post-preview" v-for="edge in $page.posts.edges" :key="edge.node.id">
           <a href="post.html">
             <h2 class="post-title">
-              Man must explore, and this is exploration at its greatest
+              {{edge.node.title}}
+              <!-- Man must explore, and this is exploration at its greatest -->
             </h2>
-            <h3 class="post-subtitle">
+            <!-- <h3 class="post-subtitle">
               Problems look mighty small from 150 miles up
-            </h3>
+            </h3> -->
           </a>
           <p class="post-meta">Posted by
-            <a href="#">Start Bootstrap</a>
-            on September 24, 2019</p>
+            <a href="#">
+              {{`${edge.node.created_by.firstname} ${edge.node.created_by.lastname}`}}
+            </a>
+            on 
+            {{edge.node.created_at}}
+          </p>
+          <p>
+            <span v-for="tag in edge.node.tags" :key="tag.id">
+              <a href="javascript:void(0)">{{tag.title}}</a>
+              &nbsp;
+            </span>
+          </p>
+          <hr>
         </div>
-        <hr>
-        <div class="post-preview">
+        <!-- <div class="post-preview">
           <a href="post.html">
             <h2 class="post-title">
               I believe every human has a finite number of heartbeats. I don't intend to waste any of mine.
@@ -43,8 +54,8 @@
             <a href="#">Start Bootstrap</a>
             on September 18, 2019</p>
         </div>
-        <hr>
-        <div class="post-preview">
+        <hr> -->
+        <!-- <div class="post-preview">
           <a href="post.html">
             <h2 class="post-title">
               Science has not yet mastered prophecy
@@ -71,22 +82,52 @@
             <a href="#">Start Bootstrap</a>
             on July 8, 2019</p>
         </div>
-        <hr>
+        <hr> -->
         <!-- Pager -->
-        <div class="clearfix">
+        <!-- <div class="clearfix">
           <a class="btn btn-primary float-right" href="#">Older Posts &rarr;</a>
-        </div>
+        </div> -->
+        <Pager :info="$page.posts.pageInfo"/>
       </div>
     </div>
   </div>
 </Layout>
 </template>
-
+<page-query>
+query ($page: Int) {
+  posts: allStrapiPost (perPage: 2, page: $page) @paginate {
+    pageInfo {
+      totalPages
+      currentPage
+    }
+    edges {
+      node {
+        id
+        title
+        content
+        tags {
+          id
+          title
+        }
+        created_by {
+          firstname
+          lastname
+        }
+        created_at
+      }
+    }
+  }
+}
+</page-query>
 <script>
+import { Pager } from 'gridsome'
 export default {
   name: 'HomePage',
   metaInfo: {
     title: 'Hello, world!'
+  },
+  components: {
+    Pager
   }
 }
 </script>
